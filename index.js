35bbdfe169f19f047d608d4a859a34e43dea199a
Sudipto Laskar <sudiptolaskar01@gmail.com>
2024-08-16 17:16:25 +0600

    added cors

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOptions ={
-   origin:['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', "https://codeupdator.vercel.app"], 
+   origin:['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', "https://codeupdator.vercel.app", "https://codeupdator-admin.vercel.app", "https://codeupdator.vercel.app"], 
    credentials:true,           
    optionSuccessStatus:200,
 }
